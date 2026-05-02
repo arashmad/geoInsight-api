ca65d04d10c9f351f7398a4d9c9edf1786b5976f
arashmad <arashmad.dev@gmail.com>
2026-05-02 20:01:08 +0200

    create db connection

---
+++ src/db/session.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from collections.abc import Generator
+
+from core.config import settings
+
+engine: Engine = create_engine(
+    settings.database_url,
+    echo=settings.database_echo,
+    pool_pre_ping=True)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False)
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
