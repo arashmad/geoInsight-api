2f3366c3345109beae3b102a50adf98985fab432
arashmad <arashmad.dev@gmail.com>
2026-05-12 19:22:25 +0200

    fix: mixing string and variable

--- src/geoinsight_api/db/models/__init__.py
+++ src/geoinsight_api/db/models/__init__.py
@@ -3,4 +3,9 @@ from geoinsight_api.db.models.project import Project
 from geoinsight_api.db.models.vector_feature import VectorFeature
 from geoinsight_api.db.models.vector_layer import VectorLayer
 
-__all__ = ["Project", "AOI", VectorFeature, VectorLayer]
+__all__ = [
+    "AOI",
+    "Project",
+    "VectorFeature",
+    "VectorLayer",
+]
