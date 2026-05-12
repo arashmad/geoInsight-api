46fcbaa8f433c6f913f767c1182e523777d6e3f3
arashmad <arashmad.dev@gmail.com>
2026-05-12 06:57:07 +0200

    register two new models to alembic

--- src/geoinsight_api/db/models/__init__.py
+++ src/geoinsight_api/db/models/__init__.py
@@ -1,4 +1,6 @@
 from geoinsight_api.db.models.aoi import AOI
 from geoinsight_api.db.models.project import Project
+from geoinsight_api.db.models.vector_feature import VectorFeature
+from geoinsight_api.db.models.vector_layer import VectorLayer
 
-__all__ = ["Project", "AOI"]
+__all__ = ["Project", "AOI", VectorFeature, VectorLayer]
