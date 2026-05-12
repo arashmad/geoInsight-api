1f7a042b3d525108886fcb768519942af81e2fd4
arashmad <arashmad.dev@gmail.com>
2026-05-12 19:07:08 +0200

    fix: extend clean-up

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,8 +15,11 @@ def db_session() -> Generator[Session, None, None]:
 
     try:
         # Cleanup before each test
+        session.execute(text("DELETE FROM vector_features"))
+        session.execute(text("DELETE FROM vector_layers"))
         session.execute(text("DELETE FROM aois"))
         session.execute(text("DELETE FROM projects"))
+
         session.commit()
 
         yield session
